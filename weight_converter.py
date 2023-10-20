93f2963f44705c1866cd2d3d725e9b73a4f4686e
Chandra Kaushik Parvatala <sparvat6@asu.edu>
2023-10-19 18:52:37 -0700

    Added a program that converts weight from lbs to kgs and vice cersa

---
+++ weight_converter.py
@@ -0,0 +1,10 @@
+weight = input('Enter weight: ')
+unit = input('(L)bs or (K)gs: ')
+unit = unit.upper()
+if unit == 'L':
+    w = 0.45 * int(weight)
+    print(f"Weight in Kg's is {w}" )
+elif unit == 'K':
+    w = 2.2 * int(weight)
+    print(f"Weight in Lbs is {w}")
+
