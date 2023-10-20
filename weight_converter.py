8d6a46ac25916e0fa471317bb0244353f89df3cb
Chandra Kaushik Parvatala <sparvat6@asu.edu>
2023-10-20 14:13:02 -0700

    reverted the changes back to original

--- weight_converter.py
+++ weight_converter.py
@@ -1,8 +1,9 @@
 weight = input('Enter weight: ')
 unit = input('(L)bs or (K)gs: ')
-if unit == 'L' or 'l':
+unit=unit.upper()
+if unit == 'L':
     w = 0.45 * int(weight)
     print(f"Weight in Kg's is {w}" )
-elif unit == 'K' or 'k':
+elif unit == 'K':
     w = 2.2 * int(weight)
     print(f"Weight in Lbs is {w}")
